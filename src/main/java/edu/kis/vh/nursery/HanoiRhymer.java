3794bd1a88e25ef5183b7c48c11f919b2f989055
Oskar Szulczewski <251231@edu.p.lodz.pl>
2026-03-12 17:16:54 +0100

    3.merge

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,15 +1,26 @@
 package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
+<<<<<<< HEAD
     int totalRejected = 0;
+=======
+
+    int totalRejected = 0;
+
+>>>>>>> format
     public int reportRejected() {
         return totalRejected;
     }
     public void countIn(int in) {
-    if (!callCheck() && in > peekaboo())
+        if (!callCheck() && in > peekaboo())
             totalRejected++;
+<<<<<<< HEAD
     else
         super.countIn(in);
 
+=======
+        else
+            super.countIn(in);
+>>>>>>> format
     }
 }
