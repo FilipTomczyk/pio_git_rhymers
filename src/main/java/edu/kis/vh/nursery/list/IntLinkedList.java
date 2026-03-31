3925a210dc90fe2c4b66367de4e99069566ed7b1
FilipTomczyk <255717@edu.p.lodz.pl>
2026-03-31 16:40:06 +0200

    14. usuniecie klasy node i przeorganizowanie projektu

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,17 +1,57 @@
+
 package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
-    Node last;
-    int i;
+    public static final int EMPTY_INDICATOR = -1;
+    private class Node {
+        private int value;
+        private Node next;
+        private Node prev;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+    }
+
+    private Node last;
+    private int i;
+
+    public Node getLast() {
+        return last;
+    }
+
+    public int getI() {
+        return i;
+    }
 
     public void push(int i) {
         if (last == null)
             last = new Node(i);
         else {
-            last.next = new Node(i);
-            last.next.prev = last;
-            last = last.next;
+            last.setNext(new Node(i));
+            last.getNext().setPrev(last);
+            last = last.getNext();
         }
     }
 
@@ -25,16 +65,16 @@ public class IntLinkedList {
 
     public int top() {
         if (isEmpty())
-            return -1;
-        return last.value;
+            return EMPTY_INDICATOR;
+        return last.getValue();
     }
 
     public int pop() {
         if (isEmpty())
-            return -1;
-        int ret = last.value;
-        last = last.prev;
+            return EMPTY_INDICATOR;
+
+        int ret = last.getValue();
+        last = last.getPrev();
         return ret;
     }
-
 }
